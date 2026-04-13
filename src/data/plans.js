a41b07cbebc41ac45483f0cc3e0c1a7c9b8fecb8
FukudaMiyasato <fukudamiyasato@gmail.com>
2026-04-13 08:05:27 -0500

    final

---
+++ src/data/plans.js
@@ -0,0 +1,71 @@
+export const plansData = [
+    {
+        name: "Opción A",
+        ambulatorio: [
+            { servicio: "Consulta Mi Doctor Amigo (Med Familiar)", deducible: 27, cobertura: "100%" },
+            { servicio: "Especialista derivado de Mi Doctor Amigo", deducible: 27, cobertura: "100%" },
+            { servicio: "Laboratorio", deducible: 0, cobertura: "15%" },
+            { servicio: "Imágenes", deducible: 0, cobertura: "15%" },
+            { servicio: "Procedimientos", deducible: 0, cobertura: "10%" },
+            { servicio: "Terapia Física", deducible: 0, cobertura: "10%" },
+            { servicio: "Endoscopía,colonoscopía", deducible: 0, cobertura: "10%" },
+            { servicio: "Servicios clínicos", deducible: 0, cobertura: "15%" },
+            { servicio: "Farmacia", deducible: "No cubre", cobertura: "No cubre" }
+        ],
+        emergencia: { deducible: 0, cobertura: "70%" },
+        hospitalario: { deducible: 350, cobertura: "70%" },
+        maternidad: "No cubre",
+        dental: "No cubre"
+    }, {
+        name: "Opción B",
+        ambulatorio: [
+            { servicio: "Consulta Mi Doctor Amigo (Med Familiar)", deducible: 27, cobertura: "100%" },
+            { servicio: "Especialista derivado de Mi Doctor Amigo", deducible: 27, cobertura: "100%" },
+            { servicio: "Laboratorio", deducible: 0, cobertura: "50%" },
+            { servicio: "Imágenes", deducible: 0, cobertura: "50%" },
+            { servicio: "Procedimientos", deducible: 0, cobertura: "50%" },
+            { servicio: "Terapia Física", deducible: 0, cobertura: "50%" },
+            { servicio: "Endoscopía,colonoscopía", deducible: 0, cobertura: "50%" },
+            { servicio: "Servicios clínicos", deducible: 0, cobertura: "50%" },
+            { servicio: "Farmacia", deducible: "No cubre", cobertura: "No cubre" }
+        ],
+        emergencia: { deducible: 0, cobertura: "90%" },
+        hospitalario: { deducible: 0, cobertura: "5%" },
+        maternidad: "No cubre",
+        dental: "No cubre"
+    }, {
+        name: "Opción C",
+        ambulatorio: [
+            { servicio: "Consulta Mi Doctor Amigo (Med Familiar)", deducible: 27, cobertura: "100%" },
+            { servicio: "Especialista derivado de Mi Doctor Amigo", deducible: 27, cobertura: "100%" },
+            { servicio: "Laboratorio", deducible: 0, cobertura: "80%" },
+            { servicio: "Imágenes", deducible: 0, cobertura: "80%" },
+            { servicio: "Procedimientos", deducible: 0, cobertura: "80%" },
+            { servicio: "Terapia Física", deducible: 0, cobertura: "80%" },
+            { servicio: "Endoscopía,colonoscopía", deducible: 0, cobertura: "80%" },
+            { servicio: "Servicios clínicos", deducible: 0, cobertura: "80%" },
+            { servicio: "Farmacia", deducible: "No cubre", cobertura: "No cubre" }
+        ],
+        emergencia: { deducible: 0, cobertura: "5%" },
+        hospitalario: { deducible: 0, cobertura: "5%" },
+        maternidad: "No cubre",
+        dental: "No cubre"
+    }, {
+        name: "Opción D",
+        ambulatorio: [
+            { servicio: "Consulta Mi Doctor Amigo (Med Familiar)", deducible: 27, cobertura: "100%" },
+            { servicio: "Especialista derivado de Mi Doctor Amigo", deducible: 27, cobertura: "100%" },
+            { servicio: "Laboratorio", deducible: 0, cobertura: "50%" },
+            { servicio: "Imágenes", deducible: 0, cobertura: "50%" },
+            { servicio: "Procedimientos", deducible: 0, cobertura: "50%" },
+            { servicio: "Terapia Física", deducible: 0, cobertura: "50%" },
+            { servicio: "Endoscopía,colonoscopía", deducible: 0, cobertura: "50%" },
+            { servicio: "Servicios clínicos", deducible: 0, cobertura: "50%" },
+            { servicio: "Farmacia", deducible: 0, cobertura: "50%" }
+        ],
+        emergencia: { deducible: 0, cobertura: "50%" },
+        hospitalario: { deducible: 0, cobertura: "5%" },
+        maternidad: "No cubre",
+        dental: "No cubre"
+    }
+];
